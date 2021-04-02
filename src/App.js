a9ad8227b5f763ff91a761ad623f3b35b215929f
Anish <sunaranish17@gmail.com>
2021-04-02 20:10:14 +0545

    Render people and plantes component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import NavBar from "./components/NavBar";
+import People from "./components/People";
+import Plantes from "./components/Plantes";
 
 function App() {
+  const [page, setPage] = useState('planets');
+
   return (
     <div className="App">
-      <h1 style={{textAlign: "center"}}>Hello BOSS!!</h1>
+      <h1 style={{ textAlign: "center" }}>Hello BOSS!!</h1>
+      <NavBar />
+      <div className="content">
+        {page === "planets" ?
+          <Plantes />
+          :
+          <People />
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <nav>
+            <button>Plantes</button>
+            <button>People</button>
+        </nav>
+    )
+}
+
+export default NavBar
