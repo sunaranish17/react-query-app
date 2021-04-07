a2b68c093503da7456bd1868ce8bcad0e0a64c65
Anish <sunaranish17@gmail.com>
2021-04-07 22:44:09 +0545

    WIP people component

--- src/components/People.js
+++ src/components/People.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import Planet from './Planet';
 
-function People() {
+const queryClient = new QueryClient();
+
+const fetchPlanets = async () => {
+    const res = await fetch('http://swapi.dev/api/planets/', { mode: 'cors' });
+    return res.json();
+}
+
+function Plantes() {
+    const { data, status } = useQuery('planets', fetchPlanets);
+    console.log("Data>>", data);
+
     return (
         <div>
-            <h1>This is people.</h1>
+            <h2>Planets.</h2>
+
+            {status === 'loading' && (
+                <div>Loading data...</div>
+            )}
+
+            {status === 'error' && (
+                <div>Error fetching data</div>
+            )}
+
+            {status === 'success' && (
+                <div>
+                    {data.results.map(planet => <Planet key={planet.name} planet={planet} />)}
+                </div>
+            )}
         </div>
     )
 }
 
-export default People
+//Higher Order Component
+const HOF = (WrappedComponent) => {
+    return (props) => (
+        <QueryClientProvider client={queryClient}>
+            <WrappedComponent {...props} />
+        </QueryClientProvider>
+    )
+}
+
+export default HOF(Plantes)
