e48e2a57decfc8f194dc4d6bc23bf3b61220ec41
Anish <sunaranish17@gmail.com>
2021-04-12 19:24:30 +0545

    Minor update

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Planet({planet}) {
+function Planet({ planet }) {
     return (
         <div className="card">
             <h3>{planet.name}</h3>
             <p>Population - {planet.population} </p>
-            <p>Terrain - {planet.terrian} </p>
+            <p>Terrain - {planet.terrain} </p>
         </div>
     );
 }
