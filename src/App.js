5353a2c282079f822a59258a54c49edc97498c27
Anish <sunaranish17@gmail.com>
2021-04-13 18:52:49 +0545

    query dev tool added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import People from "./components/People";
 import Plantes from "./components/Plantes";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 function App() {
   const [page, setPage] = useState('planets');
 
   return (
+    <>
     <div className="App">
       <h1>Hello BOSS!!</h1>
       <NavBar setPage={setPage} />
@@ -18,6 +20,8 @@ function App() {
         }
       </div>
     </div>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
 
